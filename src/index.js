5d3a836936bbf6baa7e7c30bcb187f2587b99c81
Sebastian Casal <sebastian.casal@gmail.com>
2021-11-02 17:19:14 -0300

    changing package json

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-import Config from "./config";
-import app from "./services/server";
+import Config from './config';
+import app from './services/server';
 
 let port = Config.PORT;
 
 let args;
 
 //Busco puerto por parametro, esto se repite en el middleware de FB
-process.argv.forEach((val, index, array) => {
-  array.push([index + ": " + val]);
-  return (args = array);
-});
+// process.argv.forEach((val, index, array) => {
+//   array.push([index + ": " + val]);
+//   return (args = array);
+// });
 
-if (args[2]) port = args[2];
+// if (args[2]) port = args[2];
 
 //Imprimo PID
 console.log(process.pid);
