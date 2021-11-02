d97311cffa841728f3b50c6d34407159e5cecc8b
Sebastian Casal <sebastian.casal@gmail.com>
2021-11-02 17:02:02 -0300

    testing new port config for heroku

--- src/config/index.js
+++ src/config/index.js
@@ -1,11 +1,10 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 
 dotenv.config();
 
-
 export default {
-  MONGO_ATLAS_URL: process.env.MONGO_ATLAS_URL || "mongoSRV",
-  PORT: process.env.PORT || PORT,
-  FACEBOOK_ID:process.env.FACEBOOK_ID,
-  FACEBOOK_CLIENT_ID:process.env.FACEBOOK_CLIENT_ID
+  MONGO_ATLAS_URL: process.env.MONGO_ATLAS_URL || 'mongoSRV',
+  PORT: process.env.PORT,
+  FACEBOOK_ID: process.env.FACEBOOK_ID,
+  FACEBOOK_CLIENT_ID: process.env.FACEBOOK_CLIENT_ID,
 };
